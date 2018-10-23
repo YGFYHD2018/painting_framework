6f8adab4d13da630f2b6a97eb33ebb9d082536a8
MitsuiShun <shun.mitsui@fxat.co.jp>
2018-10-23 12:09:53 +0900

    update painting_service.py

--- painting_service.py
+++ painting_service.py
@@ -1,19 +1,21 @@
 # coding: utf-8
 from flask import Flask, render_template
-from flask_cors import CORS
 from werkzeug import ImmutableDict
+import requests
 
 class FlaskWithHamlish(Flask):
     jinja_options = ImmutableDict(
         extensions=['jinja2.ext.autoescape', 'jinja2.ext.with_', 'hamlish_jinja.HamlishExtension']
     )
 app = FlaskWithHamlish(__name__) #インスタンス生成
-CORS(app)
 
 @app.route('/')
 def index():
     return render_template('index.haml')
 
 if __name__ == "__main__":
+    response = requests.post('http://127.0.0.1:5001/api/filters',data={})
+    print('response code : ')
+    print(response.status_code)
     # webサーバー立ち上げ
     app.run()
